086aafabf9e74a23e1914c621f0af68bf0e7e5f8
Mubashir Hussain <mubashrhussain41@gmail.com>
2024-12-21 12:00:10 +0500

    Setup Cron Job

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,6 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->command('send:messages')->dailyAt('12:00');
     }
 
     /**
